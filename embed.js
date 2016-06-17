be206254c9c70dac2a761cf2b30d8ac0030eb540
lailahr <lailah@AncestreStudio.local>
2016-06-17 12:08:48 +0200

    update embed example and embed.js

--- embed.js
+++ embed.js
@@ -1,8 +1,8 @@
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
-    var baseurl = "https://static.code4sa.org/forgotten-people/";
+    var baseurl = "http://code4sa.org/youthday/";
 }
-document.write('<div id="code4sa-forgotten-people-index"></div>');
+document.write('<div id="code4sa-youthday-index"></div>');
 document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
-document.write("<script>var pymParent = new pym.Parent('code4sa-forgotten-people-index', '" + baseurl + "index.html', {});</script>");
+document.write("<script>var pymParent = new pym.Parent('code4sa-youthday-index', '" + baseurl + "map.html', {});</script>");
