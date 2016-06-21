f041662ee50b69c51e8b6b906b449cdcf448c965
lailahr <lailah@AncestreStudio.local>
2016-06-21 12:02:41 +0200

    update content, temporarily change embed-example for testing

--- embed-story.js
+++ embed-story.js
@@ -1,7 +1,7 @@
 if (document.location.hostname == "localhost") {
     var baseurl = "";
 } else {
-    var baseurl = "https://static.code4sa.org/apartheid/";
+    var baseurl = "http://code4sa.org/apartheid/";
 }
 document.write('<div id="code4sa-apartheid-index"></div>');
 document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
