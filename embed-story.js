aacb040c4564dc781883aca485bd5f31a228bd57
JD Bothma <jbothma@gmail.com>
2016-07-14 14:57:34 +0200

    Only run embed code if it hasn't run before

--- embed-story.js
+++ embed-story.js
@@ -1,8 +1,13 @@
-if (document.location.hostname == "localhost") {
-    var baseurl = "";
-} else {
-    var baseurl = "https://static.code4sa.org/apartheid/";
+if (typeof(code4SAEmbedInjected) === 'undefined') {
+
+  if (document.location.hostname == "localhost") {
+      var baseurl = "";
+  } else {
+      var baseurl = "https://static.code4sa.org/apartheid/";
+  }
+  document.write('<div id="code4sa-apartheid-index"></div>');
+  document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
+  document.write("<script>var pymParent = new pym.Parent('code4sa-apartheid-index', '" + baseurl + "index.html', {});</script>");
+
+  var code4SAEmbedInjected = true;
 }
-document.write('<div id="code4sa-apartheid-index"></div>');
-document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
-document.write("<script>var pymParent = new pym.Parent('code4sa-apartheid-index', '" + baseurl + "index.html', {});</script>");
